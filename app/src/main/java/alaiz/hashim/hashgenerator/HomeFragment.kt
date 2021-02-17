698903a5d50e64c07f58e2b162df73163f80bf65
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-18 02:05:42 +0300

    title is animated

--- app/src/main/java/alaiz/hashim/hashgenerator/HomeFragment.kt
+++ app/src/main/java/alaiz/hashim/hashgenerator/HomeFragment.kt
@@ -8,24 +8,36 @@ import androidx.fragment.app.Fragment
 
 
 class HomeFragment : Fragment() {
-   private var _binding:FragmentHomeBinding?=null
-    private val binding get()=_binding!!
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding= FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
-        val hashAlgorithm=resources.getStringArray(R.array.hash_algorithm)
-        val arrayAdapter=ArrayAdapter(requireContext(),R.layout.drop_down_item,hashAlgorithm)
+        val hashAlgorithm = resources.getStringArray(R.array.hash_algorithm)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithm)
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
+        binding.generateButton.setOnClickListener {
+            animation()
+        }
+
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.home_menu,menu)
+        inflater.inflate(R.menu.home_menu, menu)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun animation() {
+        binding.titleTextView.animate().alpha(0f).duration=400
+    }
 }
