15e7e8af5d8ac5194e45a91ee8920172e1ed667a
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-19 01:14:54 +0300

    navigateToSuccessFragment function is added

--- app/src/main/java/alaiz/hashim/hashgenerator/HomeFragment.kt
+++ app/src/main/java/alaiz/hashim/hashgenerator/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -27,6 +28,7 @@ class HomeFragment : Fragment() {
         binding.generateButton.setOnClickListener {
             lifecycleScope.launch {
                 animation()
+                navigateToSuccessFragment()
             }
         }
 
@@ -69,5 +71,11 @@ class HomeFragment : Fragment() {
         delay(500)
         binding.successImageView.animate()
                 .alpha(1f).duration = 1000L
+        delay(1500)
+    }
+
+    private fun navigateToSuccessFragment(){
+        findNavController().navigate(R.id.action_homeFragment_to_successFragment)
+
     }
 }
