28bffc965f76b87cd031d0e796d5d1b6a75b0fd5
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-03-05 02:07:02 +0300

    Splash screen is added

--- app/src/main/java/alaiz/hashim/hashgenerator/MainActivity.kt
+++ app/src/main/java/alaiz/hashim/hashgenerator/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_HashGenerator)
         setContentView(R.layout.activity_main)
         navController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController)
